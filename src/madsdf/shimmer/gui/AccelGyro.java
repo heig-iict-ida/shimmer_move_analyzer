956484e99d1a7b1897f1869f2bf7a2ddf9063b13
Julien Rebetez <julien.rebetez@heig-vd.ch>
2013-01-14 19:03:06 +0100

    Use EventBus instead of listeners

--- src/madsdf/shimmer/gui/AccelGyro.java
+++ src/madsdf/shimmer/gui/AccelGyro.java
@@ -4,8 +4,8 @@ import java.util.LinkedList;
 
 /**
  * A sample received from the shimmer containing accel / gyro measurements
- * @author Gregoire Aubert
- * @version 1.0
+ * You can choose if you want calibrated or uncalibrated data by listening
+ * for the correct class on the eventbus
  */
 public class AccelGyro {
     public static class CalibratedSample {
@@ -41,6 +41,19 @@ public class AccelGyro {
             this.receivedTimestampMillis = time;
             System.arraycopy(accel, 0, this.accel, 0, 3);
             System.arraycopy(gyro, 0, this.gyro, 0, 3);
+        }
+        
+        // TODO: Remove : this is only for backward-compatibility
+        public float getVal(int i) {
+            if (i < 1 || i > 6) {
+                throw new IllegalArgumentException("Invaild i : " + i);
+            }
+            if (i <= 3) {
+                return accel[i - 1];
+            } else {
+                return accel[i - 4];
+            }
+            
         }
     }
 }
